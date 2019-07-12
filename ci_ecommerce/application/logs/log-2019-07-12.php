b5a7ebf77a31272b863c37116479049c25f8c096
urvish <urvish.patel@bytestechnolab.com>
2019-07-12 15:11:11 +0530

    changes saved

---
+++ ci_ecommerce/application/logs/log-2019-07-12.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-12 15:03:54 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:03:56 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:20 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:21 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:42 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:42 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:49 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:49 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:52 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:04:52 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:05:13 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:05:14 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:05:48 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:05:48 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:05:53 --> Could not find the language line "vendor_home_page"
+ERROR - 2019-07-12 15:06:01 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:03 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:06 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:06 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:10 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:11 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:18 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:19 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:23 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:23 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:27 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:06:28 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:07:08 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-07-12 15:07:09 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:07:10 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:41 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:42 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:49 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:50 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:53 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-07-12 15:08:54 --> 404 Page Not Found: /index
+ERROR - 2019-07-12 15:08:55 --> 404 Page Not Found: /index
