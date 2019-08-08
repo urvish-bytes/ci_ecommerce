dfd8a87a91bbc6dace44ce2e789b3d1db6d656ae
urvish <urvish.patel@bytestechnolab.com>
2019-08-08 11:33:03 +0530

    changes saved

--- csv_demo/csv.php
+++ csv_demo/csv.php
@@ -1,40 +1,53 @@
 <?php
-if(isset($_POST["import"]))
+require_once 'db_config.php';
+
+if(isset($_POST["submit"]))
 {
-	$host="localhost";
-	$db_user="root";
-	$db_password="";
-	$db='csv_demo';
-
-	$con=mysqli_connect($host,$db_user,$db_password,$db);
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-
 	$filename=$_FILES["file"]["name"];
 	if($_FILES["file"]["size"]>0)
 	{	
+		$row=1;
+		$col=0;
+
 		$file = fopen($filename, "r");
 		while (($data = fgetcsv($file, 10000, ",")) !== FALSE)
 		{
-			$sql = "INSERT into users (userid,username,password,firstname,lastname)
-            values ('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."')";
-			$result = mysqli_query($con, $sql);
-			
-			if(!empty($result))
-			{
-				echo "CSV File has been successfully Imported.";
+			if ($row) {
+				$col=$data;
 			} else {
-				header("Location: index.php");
+				$count_value = count($data);
+				for ($i=0; $i<$count_value; $i++) {
+					// trim
+					$trim = trim($count_value);
+				}
 			}
+			
+			// for($row=1; $row < $data; $row++) {
+			// 	for ($col=0; $col < $data ; $col++) { 
+			// 		$count_value = count($data);
+
+			// 		$trim = trim($count_value);
+			// 	}
+			// }
+
+			$sql = "INSERT into users (userid,username,password,firstname,lastname) values ('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."')";
+			$result = mysqli_query($con, $sql);
+		
+		}
+
+		if($result)
+		{
+			echo "CSV File has been imported successfully.";
+			// header("Location: index.php");
+		} else {
+			echo "Failed to upload file.";
+			// header("Location: index.php");
 		}
 		fclose($file);
 	}
 	else {
 		echo "Error: Please Upload only CSV File";
 	}
-
 
 }
 ?>
--- csv_demo/index.php
+++ csv_demo/index.php
@@ -2,11 +2,12 @@
 	<table border="1">
 		<h3>Import CSV file</h3>
 		<tr>
-			<td align="center">CSV File:<input type="file" name="file" id="file"></td></tr>
-			<tr>
-				<td align="center">
-					<input type="submit" name="import" value="Submit" required>
-				</td>
-			</tr>
-		</table>
-	</form>
+			<td align="center">CSV File:<input type="file" name="file" id="file" required></td>
+		</tr>
+		<tr>
+			<td align="center">
+				<input type="submit" name="submit" value="Submit">
+			</td>
+		</tr>
+	</table>
+</form>
